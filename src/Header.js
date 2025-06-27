735f162c5cedf8205555c291087d79a2e016d1f1
pritham99 <prithamjujjavarapu@gmail.com>
2025-06-26 20:59:13 -0400

    Fixed: user not defined in Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,26 +7,29 @@ import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
 
 function Header() {
-  const [{ basket }] = useStateValue();  // ✅
+  const [{ basket, user }] = useStateValue();  // ✅ Included user
+
   const handleAuthentication = () => {
     if (user) {
       auth.signOut();
     }
   };
+
   return (
     <div className="header">
       <Link to="/">
-  <img
-    className="header_logo"
-    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-    alt="Amazon logo"
-  />
-</Link>
+        <img
+          className="header_logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="Amazon logo"
+        />
+      </Link>
+
       <div className="header_search">
         <input className="header_searchInput" type="text" />
         <SearchIcon className="header_searchIcon" />
-        {/*logo*/}
       </div>
+
       <div className="header_nav">
         <Link to={!user && "/login"}>
           <div onClick={handleAuthentication} className="header_options">
@@ -38,14 +41,17 @@ function Header() {
             </span>
           </div>
         </Link>
+
         <div className="header_options">
           <span className="header_optionOne">Returns</span>
-          <span className="header_optionTwo">&Orders</span>
+          <span className="header_optionTwo">& Orders</span>
         </div>
+
         <div className="header_options">
           <span className="header_optionOne">Your</span>
           <span className="header_optionTwo">Prime</span>
         </div>
+
         <Link to="/checkout">
           <div className="header_optionBasket">
             <ShoppingBasketIcon />
